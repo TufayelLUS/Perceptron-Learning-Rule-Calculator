f657ee05a59732c01a3cb92301ee92883ea05b30
Tufayel Ahmed <39314838+TufayelLUS@users.noreply.github.com>
2019-10-20 15:13:48 +0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -23,23 +23,28 @@ while True:
         print(w)
         print("b old: " + str(b))
         iteration += 1
+        # calculate wp+b
         calc = ((w[0] * p[i][0]) + (w[1] * p[i][1])) + b
         print("n: hardlim(" + str(calc) + ")")
         a = hardlim(calc)
         print("a: " + str(a))
+        # calculate e = t-a
         e = t[i] - a
         print("e: " + str(e))
         if e != 0:
             no_error = 0
+            # calculate w_new = w_old + e * p(Transpose)
             ep = [e * p[i][0], e * p[i][1]]
             w = [w[0]+ep[0],w[1]+ep[1]]
             print("w new: ")
             print(w)
+            # calculate b_new = b_old + e
             b = b + e
             print("b new: " + str(b))
         else:
             no_error += 1
             if no_error == 3:
+                # 3 input, 3 consecutive matches
                 print("Final Answer: ")
                 print("Weight:")
                 print(w)
